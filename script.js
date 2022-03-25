f78b774732a16625fe8eda997419771de800b14f
Your Name <you@example.com>
2022-03-24 20:02:47 -0400

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addItem(){
     var ul = document.getElementById("dynamic-list");
     var candidate = document.getElementById("candidate");
     var li = document.createElement("li");
-    li.setAttribute('id',candidate.value);
+   // li.setAttribute('id',candidate.value);
     li.appendChild(document.createTextNode(candidate.value));
     ul.appendChild(li);
 }
